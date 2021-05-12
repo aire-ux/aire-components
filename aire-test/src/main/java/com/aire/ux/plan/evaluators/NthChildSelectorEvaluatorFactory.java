c51b82090fabe55cf30a3305121f3d9dd566729e
Josiah Haswell <josiah.haswell@gmail.com>
2021-05-12 13:07:09 -0600

    Adding support for nth child selector with scalar argument

--- aire-test/src/main/java/com/aire/ux/plan/evaluators/NthChildSelectorEvaluatorFactory.java
+++ aire-test/src/main/java/com/aire/ux/plan/evaluators/NthChildSelectorEvaluatorFactory.java
@@ -5,7 +5,12 @@ import com.aire.ux.parsers.ast.SyntaxNode;
 import com.aire.ux.plan.Evaluator;
 import com.aire.ux.plan.EvaluatorFactory;
 import com.aire.ux.plan.PlanContext;
+import com.aire.ux.select.css.CssSelectorToken;
 import com.aire.ux.select.css.Token;
+import com.aire.ux.test.NodeAdapter;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import lombok.val;
 
 public class NthChildSelectorEvaluatorFactory implements EvaluatorFactory {
 
@@ -17,5 +22,65 @@ public class NthChildSelectorEvaluatorFactory implements EvaluatorFactory {
 
   @Override
   public Evaluator create(SyntaxNode<Symbol, Token> node, PlanContext context) {
+    return new NthChildSelectorEvaluator(node, context);
+  }
+
+  static class NthChildSelectorEvaluator implements Evaluator {
+
+    final Evaluator delegate;
+    final PlanContext context;
+    final SyntaxNode<Symbol, Token> node;
+    public NthChildSelectorEvaluator(SyntaxNode<Symbol, Token> node, PlanContext context) {
+      this.node = node;
+      this.context = context;
+      this.delegate = detectDelegate(node);
+    }
+
+
+
+    @Override
+    public <T> Set<T> evaluate(Set<T> workingSet, NodeAdapter<T> hom) {
+      return delegate.evaluate(workingSet, hom);
+    }
+
+    static Evaluator detectDelegate(SyntaxNode<Symbol, Token> node)  {
+      if(isScalarNumber(node)) {
+        return new ScalarEvaluator(node);
+      }
+      return null;
+    }
+
+    private static boolean isScalarNumber(SyntaxNode<Symbol, Token> node) {
+      val children = node.getChildren();
+      return children.size() == 1 && children.get(0).getSource().getType() == CssSelectorToken.Numeric;
+    }
+  }
+
+
+  static class ScalarEvaluator implements Evaluator {
+    final int offset;
+    final SyntaxNode<Symbol, Token> node;
+
+    ScalarEvaluator(SyntaxNode<Symbol, Token> node) {
+      this.node = node;
+      this.offset = readOffset();
+    }
+
+    private int readOffset() {
+      val child = node.removeChild(0);
+      return Integer.parseInt(child.getSource().getLexeme());
+    }
+
+    @Override
+    public <T> Set<T> evaluate(Set<T> workingSet, NodeAdapter<T> hom) {
+      val results = new LinkedHashSet<T>();
+      for(val item : workingSet) {
+        val children = hom.getChildren(item);
+        if(offset >= 0 && offset < children.size()) {
+          results.add(children.get(offset));
+        }
+      }
+      return results;
+    }
   }
 }
--- aire-test/src/test/java/com/aire/ux/plan/evaluators/FunctionApplicationEvaluatorFactoryTest.java
+++ aire-test/src/test/java/com/aire/ux/plan/evaluators/FunctionApplicationEvaluatorFactoryTest.java
@@ -1,6 +1,7 @@
 package com.aire.ux.plan.evaluators;
 
 import static com.aire.ux.test.Nodes.node;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.aire.ux.plan.EvaluatorFactory;
 import lombok.val;
@@ -11,11 +12,38 @@ class FunctionApplicationEvaluatorFactoryTest extends EvaluatorFactoryTestCase {
   @Test
   void ensureFunctionApplicationWorks() {
     val node = node("html").child(
-        node("body")
+        node("body").child(node("div"))
     );
 
     val result = eval("html:nth-child(0)", node);
+    System.out.println(result);
+  }
 
+  @Test
+  void ensureNestedSelectionsWork() {
+    val node = node("html").child(
+        node("body").children(
+            node("section")
+                .children(
+                    node("div").attribute("class", "select")
+                        .children(
+                            node("span"),
+                            node("ul")
+                                .children(
+                                    node("li").attribute("first", "true"),
+                                    node("li").attribute("second", "true"),
+                                    node("li").attribute("third", "true")
+                                ))
+                ),
+            node("article").attribute("class", "select").children(
+                node("span").attribute("first", "true"),
+                node("span").attribute("second", "true")
+
+            )
+        )
+    );
+    val results = eval("html > body *.select:nth-child(1)", node);
+    assertEquals(2, results.size());
   }
 
 
