e453e5003a86b803928314b6cf6f433f4d5e0647
Josiah Haswell <josiah.haswell@gmail.com>
2021-05-18 11:40:20 -0600

    Adding test

--- aire-test/src/test/java/com/aire/ux/plan/evaluators/NthChildSelectorEvaluatorFactoryTest.java
+++ aire-test/src/test/java/com/aire/ux/plan/evaluators/NthChildSelectorEvaluatorFactoryTest.java
@@ -32,6 +32,19 @@ class NthChildSelectorEvaluatorFactoryTest extends EvaluatorFactoryTestCase {
     val result = eval(":nth-child(2n+1)", node);
     assertEquals(2, result.size());
   }
+
+  @Test
+  void ensureNegativeIndexesWork() {
+    val node = node("ul").children(
+        node("li").attribute("a", "1"),
+        node("li").attribute("a", "2"),
+        node("li").attribute("a", "3")
+    );
+
+    val result = eval(":nth-child(-n+3)", node);
+    assertEquals(3, result.size());
+  }
+
   @Override
   protected EvaluatorFactory createFactory() {
     return new NthChildSelectorEvaluatorFactory();
