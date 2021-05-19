77da258cabdc46293cb8399ffc8c7afb9e45fbcc
Josiah Haswell <josiah.haswell@gmail.com>
2021-05-19 10:55:36 -0600

    Adding tests

--- aire-test/src/test/java/com/aire/ux/plan/evaluators/SelectorUnionEvaluatorFactoryTest.java
+++ aire-test/src/test/java/com/aire/ux/plan/evaluators/SelectorUnionEvaluatorFactoryTest.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.aire.ux.plan.EvaluatorFactory;
 import lombok.val;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SelectorUnionEvaluatorFactoryTest extends EvaluatorFactoryTestCase {
 
-  @Test
-  void ensureSelectorSelectsSimpleUnion() {
+  @ParameterizedTest
+  @ValueSource(strings = {"ul, span > cool, span", "ul,span>cool,span", "ul  , span  > cool, span"})
+  void ensureSelectorSelectsSimpleUnion(String expression) {
     val node =
         node("html").children(node("ul"), node("span").children(node("cool")), node("frapper"));
-    val results = eval("ul, span > cool, span", node);
+    val results = eval(expression, node);
     assertEquals(3, results.size());
     assertContainsTypes(results, "ul", "span", "cool");
   }
