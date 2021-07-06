e39ccd0260e11ce142e6466e124641cd90687cad
Josiah Haswell <josiah.haswell@gmail.com>
2021-07-06 13:44:01 -0600

    Adding support for text nodes

--- aire-ux-flow/controls/aire-button/src/main/java/com/aire/ux/control/Button.java
+++ aire-ux-flow/controls/aire-button/src/main/java/com/aire/ux/control/Button.java
@@ -2,11 +2,42 @@ package com.aire.ux.control;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.HasText;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.dom.Element;
+import lombok.val;
 
 @Tag("aire-button")
 @JsModule("@aire-ux/aire-button")
 @NpmPackage(value = "@aire-ux/aire-button", version = "0.0.14")
-public class Button extends Component implements HasStyle {}
+public class Button extends Component implements HasStyle, HasText {
+
+  /** create an empty button */
+  public Button() {}
+
+  /** @param text the text to instantiate this button with */
+  public Button(String text) {
+    setText(text);
+  }
+
+  public void setText(String text) {
+    removeTextNodes();
+    if (text != null) {
+      val element = Element.createText("text");
+      //      element.setAttribute("slot", "");
+      getElement().appendChild(element);
+    }
+  }
+
+  private void removeTextNodes() {
+    for (int i = 0; i < getElement().getChildCount(); i++) {
+      val child = getElement().getChild(i);
+      if (child.isTextNode()) {
+        //        child.removeAttribute("slot");
+        getElement().removeChild(i);
+      }
+    }
+  }
+}
--- aire-ux-flow/theme/aire-theme/src/main/java/com/aire/ux/theme/context/ThemeChangeListener.java
+++ aire-ux-flow/theme/aire-theme/src/main/java/com/aire/ux/theme/context/ThemeChangeListener.java
@@ -14,6 +14,7 @@ import io.sunshower.lang.events.EventType;
 import java.util.List;
 import java.util.logging.Level;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.val;
@@ -101,6 +102,7 @@ public class ThemeChangeListener implements EventListener<Theme> {
     }
   }
 
+  @Data
   @NoArgsConstructor
   @AllArgsConstructor
   public static class StyleDefinition {
