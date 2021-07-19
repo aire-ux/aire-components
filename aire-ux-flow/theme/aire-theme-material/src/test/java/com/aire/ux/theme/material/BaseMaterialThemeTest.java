64be4d4fba2e9525f9d615a097b066a6cadf4218
Josiah Haswell <josiah.haswell@gmail.com>
2021-07-19 17:20:52 -0600

    All tests are running

--- aire-ux-flow/theme/aire-theme-material/src/test/java/com/aire/ux/theme/material/BaseMaterialThemeTest.java
+++ aire-ux-flow/theme/aire-theme-material/src/test/java/com/aire/ux/theme/material/BaseMaterialThemeTest.java
@@ -4,20 +4,22 @@ import static org.mockito.Mockito.spy;
 
 import com.aire.ux.test.AireTest;
 import com.aire.ux.test.Context;
+import com.aire.ux.test.ViewTest;
 import com.aire.ux.test.spring.EnableSpring;
 import com.aire.ux.test.themes.EnableThemes;
 import com.aire.ux.theme.ThemeDefinition;
 import com.aire.ux.theme.context.ThemeChangeListener;
 import com.vaadin.flow.component.UI;
 import lombok.val;
-import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
 
 @AireTest
 @EnableSpring
 @EnableThemes
+@ContextConfiguration(classes = Cfg.class)
 class BaseMaterialThemeTest {
 
-  @Test
+  @ViewTest
   void ensureThemeServletServesResourcesCorrectly(@Context UI ui) {
     val listener = new ThemeChangeListener(() -> spy(ui));
   }
---
+++ aire-ux-flow/theme/aire-theme-material/src/test/java/com/aire/ux/theme/material/Cfg.java
@@ -0,0 +1,6 @@
+package com.aire.ux.theme.material;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Cfg {}
