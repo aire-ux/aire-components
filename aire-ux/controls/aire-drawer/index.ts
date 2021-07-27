f0bd152dd469642c8a28d43d267d68225e5a5602
Josiah Haswell <josiah.haswell@gmail.com>
2021-07-27 15:58:26 -0600

    Adding aire-drawer skeleton

--- aire-ux/controls/aire-drawer/index.ts
+++ aire-ux/controls/aire-drawer/index.ts
@@ -1,1 +1,1 @@
-export { AireButton } from './src/AireButton.js';
+export { AireDrawer } from './src/AireDrawer.js';
--- aire-ux/controls/aire-drawer/src/AireDrawer.ts
+++ aire-ux/controls/aire-drawer/src/AireDrawer.ts
@@ -1,10 +1,10 @@
 import {css, customElement, html, LitElement, property,} from 'lit-element';
 
 // @ts-ignore
-import {styles} from '../themes/base/aire-button'
+import {styles} from '../themes/base/aire-drawer'
 
-@customElement('aire-button')
-export class AireButton extends LitElement {
+@customElement('aire-drawer')
+export class AireDrawer extends LitElement {
 
 
   static get styles() {
@@ -33,9 +33,7 @@ export class AireButton extends LitElement {
 
   render() {
     return html`
-      <button>
-        <slot></slot>
-      </button>
+      <div>aire-drawer</div>
     `;
 
   }
