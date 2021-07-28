3f8e53277b0a050cc4820321697bc0d0a8a9dea4
Josiah Haswell <josiah.haswell@gmail.com>
2021-07-28 11:24:28 -0600

    Fixing overlay and references

---
+++ aire-ux/controls/aire-overlay/src/AireOverlay.ts
@@ -0,0 +1,40 @@
+import {css, customElement, html, LitElement, property,} from 'lit-element';
+
+// @ts-ignore
+import {styles} from '../themes/base/aire-expansion'
+
+@customElement('aire-expansion')
+export class AireOverlay extends LitElement {
+
+
+  static get styles() {
+    return css`${styles}`;
+  }
+
+
+  /**
+   * set the style classes for this element
+   */
+  @property({
+    reflect: true,
+    attribute: true
+  })
+  classes: String;
+
+  /**
+   * set the type of this button
+   */
+  @property({
+    reflect: true,
+    attribute: true
+  })
+  type: String;
+
+
+  render() {
+    return html`
+      <div>overlay</div>
+    `;
+
+  }
+}
