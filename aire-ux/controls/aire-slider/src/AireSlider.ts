f855bdc44279feb39acd681baf56ad4ecaf67931
Josiah Haswell <josiah.haswell@gmail.com>
2021-07-28 12:09:00 -0600

    Adding slider

--- aire-ux/controls/aire-slider/src/AireSlider.ts
+++ aire-ux/controls/aire-slider/src/AireSlider.ts
@@ -1,10 +1,10 @@
 import {css, customElement, html, LitElement, property,} from 'lit-element';
 
 // @ts-ignore
-import {styles} from '../themes/base/aire-button'
+import {styles} from '../themes/base/aire-slider'
 
-@customElement('aire-button')
-export class AireButton extends LitElement {
+@customElement('aire-slider')
+export class AireSlider extends LitElement {
 
 
   static get styles() {
@@ -33,9 +33,7 @@ export class AireButton extends LitElement {
 
   render() {
     return html`
-      <button>
-        <slot></slot>
-      </button>
+      <div>slider</div>
     `;
 
   }
