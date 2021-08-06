54a3b518b8403a83a66468bf0f4326a23317d2d1
Josiah Haswell <josiah.haswell@gmail.com>
2021-08-06 14:25:26 -0600

    Button basically works--need to add theme

--- aire-ux-demos/aire-ux-flow-demo/src/main/java/com/example/demo/GreetService.java
+++ aire-ux-demos/aire-ux-flow-demo/src/main/java/com/example/demo/GreetService.java
@@ -2,11 +2,11 @@ package com.example.demo;
 
 public class GreetService {
 
-    public String greet(String name) {
-        if (name == null || name.isEmpty()) {
-            return "Hello anonymous user";
-        } else {
-            return "Hello " + name;
-        }
+  public String greet(String name) {
+    if (name == null || name.isEmpty()) {
+      return "Hello anonymous user";
+    } else {
+      return "Hello " + name;
     }
+  }
 }
