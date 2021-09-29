75ea7c90c0897e3d3386669f3b70fbceed92d7bb
Josiah Haswell <josiah.haswell@gmail.com>
2021-09-29 17:21:47 -0600

    Adding typings

--- aire-ux/controls/aire-designer/src/AireDesigner.ts
+++ aire-ux/controls/aire-designer/src/AireDesigner.ts
@@ -1,40 +1,36 @@
-import {css, customElement, html, LitElement, property,} from 'lit-element';
-
-// @ts-ignore
-import {styles} from '../themes/base/aire-designer'
+import {
+  css,
+  customElement,
+  html,
+  LitElement,
+  property,
+  PropertyValues
+} from 'lit-element';
+import {Designer} from "./designer/core/designer";
 
 @customElement('aire-designer')
 export class AireDesigner extends LitElement {
 
+  private designer: Designer;
 
-  static get styles() {
-    return css`${styles}`;
-  }
-
-
-  /**
-   * set the style classes for this element
-   */
-  @property({
-    reflect: true,
-    attribute: true
-  })
-  classes: String;
-
-  /**
-   * set the type of this button
-   */
-  @property({
-    reflect: true,
-    attribute: true
-  })
-  type: String;
 
 
   render() {
     return html`
-      <div>aire-designer</div>
+      <div 
+          class="aire-designer-container">
+      </div>
     `;
+  }
 
+
+  protected firstUpdated(_changedProperties: PropertyValues) {
+    super.firstUpdated(_changedProperties);
+    new Designer(this.renderRoot.firstElementChild as Element);
+
+  }
+
+  protected createRenderRoot(): Element | ShadowRoot {
+    return this;
   }
 }
---
+++ aire-ux/controls/aire-designer/typings/mxgraph/index.d.ts
@@ -0,0 +1,30 @@
+
+
+declare module "mxgraph" {
+
+
+
+
+
+  global {
+
+    // @ts-ignore
+    const mxGraph: typeof mxGraph;
+    // @ts-ignore
+    const mxClient: typeof mxClient;
+
+    // @ts-ignore
+    const mxGraphModel: typeof mxGraphModel;
+
+    // @ts-ignore
+    const mxUtils: typeof mxUtils;
+    // @ts-ignore
+    const mxEvent: typeof mxEvent;
+    // @ts-ignore
+    const mxRubberband: typeof mxRubberband;
+  }
+
+
+}
+
+
