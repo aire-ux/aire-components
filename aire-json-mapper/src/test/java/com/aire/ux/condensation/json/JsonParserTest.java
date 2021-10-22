4ae01913135fb65cd3a8b765477cb00b08b0f8f6
Josiah Haswell <josiah.haswell@gmail.com>
2021-10-22 14:08:12 -0600

    Adding tests for parsing array

--- aire-json-mapper/src/test/java/com/aire/ux/condensation/json/JsonParserTest.java
+++ aire-json-mapper/src/test/java/com/aire/ux/condensation/json/JsonParserTest.java
@@ -1,5 +1,8 @@
 package com.aire.ux.condensation.json;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.aire.ux.condensation.json.Value.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.NoSuchElementException;
@@ -8,6 +11,31 @@ import lombok.val;
 import org.junit.jupiter.api.Test;
 
 public class JsonParserTest {
+
+  @Test
+  void ensureParsingArrayWithObjectValuesWorks() {
+    val obj = "\n"
+              + "[\n"
+              + "  {\n"
+              + "    \"firstName\": \"Josiah\",\n"
+              + "    \"lastName\": \"Haswell\"\n"
+              + "  },\n"
+              + "  {\n"
+              + "    \"firstName\": \"Lisa\",\n"
+              + "    \"lastName\": \"Gumerman\"\n"
+              + "  }\n"
+              + "]";
+
+    val ast = new JsonParser().parse(obj);
+    assertEquals(ast.getRoot().getValue().getType(), Type.Array);
+    System.out.println(ast);
+  }
+  @Test
+  void ensureParsingArraysWorks() {
+    val obj = "[1, 2, 3]";
+    val ast = new JsonParser().parse(obj);
+    assertEquals(ast.getRoot().getValue().getType(), Type.Array);
+  }
 
   @Test
   void ensureParsingStringsWorks() {
