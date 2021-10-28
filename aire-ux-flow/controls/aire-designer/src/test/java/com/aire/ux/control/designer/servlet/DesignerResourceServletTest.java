98128b243560ba1925429a9b97e1ce5dcd54bff4
Josiah Haswell <josiah.haswell@gmail.com>
2021-10-28 13:43:46 -0600

    Disabling test

--- aire-ux-flow/controls/aire-designer/src/test/java/com/aire/ux/control/designer/servlet/DesignerResourceServletTest.java
+++ aire-ux-flow/controls/aire-designer/src/test/java/com/aire/ux/control/designer/servlet/DesignerResourceServletTest.java
@@ -3,8 +3,10 @@ package com.aire.ux.control.designer.servlet;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import lombok.val;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@Disabled
 class DesignerResourceServletTest {
 
   @Test
