327315fab24e8f1eba7654b62b44b341fcaea57e
Josiah Haswell <josiah.haswell@gmail.com>
2022-02-15 09:28:01 -0700

    Caching component decorators to avoid reinstantiation

--- aire-ui/src/main/java/io/sunshower/zephyr/aire/ActionDecoratingComponentDecorator.java
+++ aire-ui/src/main/java/io/sunshower/zephyr/aire/ActionDecoratingComponentDecorator.java
@@ -6,20 +6,12 @@ import lombok.NonNull;
 
 public class ActionDecoratingComponentDecorator implements ComponentDecorator {
 
-  public ActionDecoratingComponentDecorator() {
-
-    System.out.println("Inss");
-  }
 
   @Override
   public void onComponentEntered(@NonNull HasElement component) {
-    //    ComponentDecorator.super.onComponentEntered(component);
-    System.out.println("Entered" + component);
   }
 
   @Override
   public void onComponentExited(@NonNull HasElement component) {
-    System.out.println("Exited" + component);
-    //    ComponentDecorator.super.onComponentExited(component);
   }
 }
