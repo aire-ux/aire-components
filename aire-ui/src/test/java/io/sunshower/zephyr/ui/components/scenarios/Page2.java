58c18ce4b4e4c0eb94550ab65ab5977ec6ec2a47
Josiah Haswell <josiah.haswell@gmail.com>
2022-03-24 18:47:58 -0600

    Fixing some issues

--- aire-ui/src/test/java/io/sunshower/zephyr/ui/components/scenarios/Page2.java
+++ aire-ui/src/test/java/io/sunshower/zephyr/ui/components/scenarios/Page2.java
@@ -1,6 +1,7 @@
 package io.sunshower.zephyr.ui.components.scenarios;
 
 import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.textfield.TextField;
 import io.sunshower.zephyr.spring.Dynamic;
 import io.sunshower.zephyr.ui.components.Wizard;
 import io.sunshower.zephyr.ui.components.WizardModel;
@@ -13,11 +14,21 @@ public class Page2 extends Section {
 
   @Getter private final Wizard<?> wizard;
   @Getter private final WizardModel model;
+  private TextField textField;
 
   @Dynamic
   public Page2(@Dynamic Wizard<?> parent, @Dynamic WizardModel model) {
     addClassName("page-2");
     this.model = Objects.requireNonNull(model);
     this.wizard = Objects.requireNonNull(parent);
+    configure();
   }
+
+  private void configure() {
+    textField = new TextField();
+    textField.getElement().getClassList().add("text-field");
+    add(textField);
+  }
+
+
 }
